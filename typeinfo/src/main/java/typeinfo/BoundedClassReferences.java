8f86757c655c193edd60efc9c3da744dbff0f1b4
YaoYunZhe <yaoyunzhe_mail@163.com>
2017-04-11 21:44:22 +0800

    [Add]typeinfo-0411

--- typeinfo/src/main/java/typeinfo/BoundedClassReferences.java
+++ typeinfo/src/main/java/typeinfo/BoundedClassReferences.java
@@ -1,10 +1,13 @@
 package typeinfo;//: typeinfo/BoundedClassReferences.java
 
 public class BoundedClassReferences {
-  public static void main(String[] args) {
-    Class<? extends Number> bounded = int.class;
-    bounded = double.class;
-    bounded = Number.class;
-    // Or anything else derived from Number.
-  }
+    public static void main(String[] args) {
+        // Integer虽然继承Number，但是Integer Class 不是Number Class对象的子类
+//        Class<Number> genericIntClass2 = int.class;// Illegal
+
+        Class<? extends Number> bounded = int.class;
+        bounded = double.class;
+        bounded = Number.class;
+        // Or anything else derived from Number.
+    }
 } ///:~
--- typeinfo/src/main/java/typeinfo/PetCount2.java
+++ typeinfo/src/main/java/typeinfo/PetCount2.java
@@ -1,8 +1,9 @@
 package typeinfo;//: typeinfo/PetCount2.java
-import typeinfo.pets.*;
+
+import typeinfo.pets.Pets;
 
 public class PetCount2 {
-  public static void main(String[] args) {
-    PetCount.countPets(Pets.creator);
-  }
+    public static void main(String[] args) {
+        PetCount.countPets(Pets.creator);
+    }
 } /* (Execute to see output) *///:~
